bb13dcbbb6a58960c251b47ff40a6232aeb48c9c
Kyle Koder <silvertallon@gmail.com>
2020-09-11 13:24:25 -0700

    Empty client-side mod with flib

---
+++ src/main/java/com/fantasticsource/tiamattooltips/TiamatTooltips.java
@@ -0,0 +1,32 @@
+package com.fantasticsource.tiamattooltips;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+@Mod(modid = TiamatTooltips.MODID, name = TiamatTooltips.NAME, version = TiamatTooltips.VERSION, acceptableRemoteVersions = "*", dependencies = "required-after:fantasticlib@[1.12.2.036y,)")
+public class TiamatTooltips
+{
+    public static final String MODID = "tiamattooltips";
+    public static final String NAME = "Tiamat Tooltips";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(TiamatTooltips.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
